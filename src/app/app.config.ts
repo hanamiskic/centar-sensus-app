51c6c90ad99ba8d3eff3fcaec1e03eb9ab898f6f
Hana Miškić <hana.miskic@gmail.com>
2025-07-30 00:03:53 +0200

    Initial Firebase setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,18 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+import { provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp } from 'firebase/app';
+import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDPM7akstKRowvCTbVGQNlsPXptKEqrngU",
+    authDomain: "centar-sensus.firebaseapp.com",
+    projectId: "centar-sensus",
+    storageBucket: "centar-sensus.firebasestorage.app",
+    messagingSenderId: "824277428467",
+    appId: "1:824277428467:web:62c4bb1ab6525e5f89ef86"
+  },
+};
